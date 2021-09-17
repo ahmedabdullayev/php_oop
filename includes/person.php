f5d3bf0ad7f12f93a6195e5046cf75992eeb2ebf
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-09-17 18:45:52 +0300

    constructor

--- includes/person.php
+++ includes/person.php
@@ -7,12 +7,24 @@ class Person
     public  $eyeColor;
     public  $age;
 
+    //constructor
+    public function __construct($name, $eyeColor, $age){
+        $this->name = $name;
+        $this->eyeColor = $eyeColor;
+        $this->age = $age;
+    }
+
     // Methods
     public function setName($name){
         $this->name = $name;
     }
     public function getName(){
         return $this->name;
+    }
+    //desctructor
+    public function __destruct()
+    {
+
     }
 }
 
--- index.php
+++ index.php
@@ -2,12 +2,10 @@
 include "includes/person.php";
 ?>
 <?php
-    $person1 = new Person();
-    $person1->setName("Ahmed");
+    $person1 = new Person("Ahmed", "Blue", 21);
     echo $person1->getName();
+    echo $person1->eyeColor;
+    $person1->setName("Jogy");
     echo $person1->name;
 
-    $person2 = new Person();
-    $person2->setName("Timmy");
-echo $person2->name;
 ?>
